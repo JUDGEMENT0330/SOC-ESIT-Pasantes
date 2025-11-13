01916db29f9c132432d0133304dea903642cab8a
Alex Mancia <142165108+JUDGEMENT0330@users.noreply.github.com>
2025-11-13 15:28:54 -0600

    Update types.ts

--- types.ts
+++ types.ts
@@ -63,6 +63,10 @@ export interface SimulationState {
     db_config_permissions: string;
     hydra_run_count: number;
     server_load: number;
+    red_terminal_output: TerminalLine[];
+    blue_terminal_output: TerminalLine[];
+    red_prompt: PromptState;
+    blue_prompt: PromptState;
     last_updated?: string;
 }
 
