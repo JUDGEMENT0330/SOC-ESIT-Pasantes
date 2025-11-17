0f572656849effbf09877998749358a4bcd55aca
Alex Mancia <142165108+JUDGEMENT0330@users.noreply.github.com>
2025-11-17 11:46:25 -0600

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,9 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-// Placeholder credentials to allow the application to run in this environment.
-// In a real deployment, these should be configured as environment variables.
-const supabaseUrl = 'https://ivmrevmvnbpijxbktsgw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2bXJldm12bmJwaWp4Ymt0c2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0MjU4MjIsImV4cCI6MjAzNjkxMTgyMn0.5b2Tz0-aM6hDbzG7Yw_5599_2Zk4fK4iXU-B5b_qF_k';
+const supabaseUrl = 'https://mxhnkoxuoogorhynygdi.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14aG5rb3h1b29nb3JoeW55Z2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4OTg4NzAsImV4cCI6MjA3ODQ3NDg3MH0.hQDaN09JMBhXr0-3IbAZ4shsjR6x0JP8eUxtRqUGA0s'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
