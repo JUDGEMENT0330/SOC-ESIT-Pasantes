63a3864886ff49da95b845d77cbd537965e4817d
Alex Mancia <142165108+JUDGEMENT0330@users.noreply.github.com>
2025-11-17 11:18:12 -0600

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,12 @@
 
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mxhnkoxuoogorhynygdi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14aG5rb3h1b29nb3JoeW55Z2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4OTg4NzAsImV4cCI6MjA3ODQ3NDg3MH0.hQDaN09JMBhXr0-3IbAZ4shsjR6x0JP8eUxtRqUGA0s'
+const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be provided in environment variables.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
